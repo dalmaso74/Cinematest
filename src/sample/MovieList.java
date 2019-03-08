cecedd0675532c990a5bf7b87b9149811d172d64
dalmaso74 <43674564+dalmaso74@users.noreply.github.com>
2019-03-08 17:33:37 +0000

    WOrk2

---
+++ src/sample/MovieList.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+import java.util.ArrayList;
+
+public class MovieList {
+    ArrayList <Movie> Films= new ArrayList<>();
+
+
+    //@FXML
+    //void
+
+
+}
